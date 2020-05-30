9b8f1d67a0178587ac7c3f9cea5de03cf05146b8
bwalwyn <bwalwyn@gmail.com>
2020-05-30 18:39:58 +0200

    single sms

--- singlesms.py
+++ singlesms.py
@@ -19,3 +19,22 @@ import numpy as np
 # africas talking API client
 import africastalking
 
+# USERNAME = "atyellowuganda"
+# API_KEY = "ac2d7a5054b5f4e3e25fda1f2634ce80fb9bb81386ac06fe9c9c3743e2517f80"
+# SENDER = None
+
+USERNAME = "newshirt"
+API_KEY = "9b0e40cf471546d8b3564cd685ad74f1a3490c9702947c5633dd84d774b37868"
+SENDER = "YELLOW"
+
+africastalking.initialize(USERNAME, API_KEY)
+
+sms = africastalking.SMS
+response = sms.send(
+        message = "Hi Cy, Please confirm this message with Ben", 
+        recipients = ['+265999576414'],
+        sender_id = SENDER,
+        )
+
+print(response)
+
